79aff79fa9489956a003a01dcd11af341144c734
ÜLKEM ERCİN <ulkemercin97@gmail.com>
2024-12-11 18:05:56 +0300

    last changes

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitiviity = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitiviity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitiviity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
